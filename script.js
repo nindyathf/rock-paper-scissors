f9fa28de47d1fd6c03251ba0b98465912a899527
Nindya Athifa <168733133+nindyathf@users.noreply.github.com>
2026-02-24 10:30:36 +0700

    exercise submit

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let x = Math.random();
+    if (x < 0.33) {
+        return "rock";
+    } else if (x <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
